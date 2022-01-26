ccd0d0780eb78ab1e241eccf007d4f0df7279c74
koras <kos6476@yandex.ru>
2022-01-26 19:29:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,270 @@
+//let result = confirm('Удалять ли базу данных?');
+
+// if (result) {
+//     console.log('здесь исполняем если результат положительный');
+//     // здесь исполняем если результат положительный
+// } else {
+//     console.log('здесь исполняем если результат отрицательный');
+
+// }
+
+//debugger;
+
+
+// old function ie6;
+// getContent = function() {
+//         /// здесь наш новый код
+
+//         getContentBody();
+//     }
+//     // // new function ie10;
+
+
+
+// getContentBody();
+
+
+
+
+// ie6;
+// ie10;
+
+
+//console.log('test');
+
+
+
+// let object = {
+//     function: [
+//         function() {
+//             this
+
+//             let data = 1;
+//             console.log('получение данных из базы');
+//             data = data + 1;
+//         },
+//         function() {
+//             console.log('обпработка данных');
+//         },
+//         function() {
+//             console.log('добавление кнопок');
+//         },
+//         function() {
+//             console.log('отправка данных');
+
+//         },
+
+//     ]
+// }
+
+
+// let dataObject = {
+//     carrot: [{ w: 10, h: 34 }, { w: 25, h: 50 }, ],
+//     apple: 10,
+//     lemon: 51,
+// };
+
+
+// dataObject.functionName = (data) => {
+//     console.log('стрелочная функция')
+// }
+
+// // while (object.function) {
+// //     object.function[t]()
+
+// // }
+
+
+
+
+// dataObject.getFunction = function(param) {
+//     console.log(param);
+// }
+
+// dataObject.getWidth = function(w, h) {
+//     console.log(w, h);
+// }
+
+// dataObject.createCube = function(w, h, l) {
+//     console.log(w, h, l);
+// }
+
+// dataObject.getFunction('love');
+
+
+
+
+
+
+
+
+
+// let value = "Маша";
+
+// let text1 = "Привет " + value;
+
+// let text2 = `Привет ${value} ${1+1} `;
+// let text3 = "Привет ${value} ";
+// let text4 = 'Привет ${value} ';
+
+
+// console.log(text2);
+// console.log(text3);
+// console.log(text4);
+
+
+
+// let result = true;
+// let mut = 95;
+
+// function getResult() {
+//     // return result || mut == 96 && 45 != 66;
+
+
+//     let total = function total() {
+//         console.log(1111);
+
+
+//     }
+
+// }
+
+
+
+
+// let dataArray = [
+//     'apple',
+//     'carrot',
+//     'lemon'
+// ];
+
+// console.log(dataArray[0])
+// console.log(dataArray[1])
+// console.log(dataArray[2])
+
+// let num = 3;
+
+// switch (num) {
+//     case 2:
+//         {
+//             console.log('два');
+//         }
+//         break;
+//     case "3":
+//         {
+//             console.log('ттри');
+//         }
+//         //     break;
+//     case 4:
+//         {
+//             console.log('четыре');
+//         }
+//         break;
+//     default:
+//         {
+
+//             console.log('нет числа');
+//         }
+
+// }
+
+// // let p = 0;
+
+// // //do {
+// // console.log(p, ' = ', dataObject.carrot[p]);
+// // p++;
+// // }
+// // while (dataObject.carrot)
+
+// // for (let m = 0; m < dataObject.carrot.length; m++) {
+
+// //     console.log(m, ' = ', dataObject.carrot[m]);
+// // }
+
+
+// elem2.onclick = function() { // перезапишет существующий обработчик
+//     alert('Станет'); // выведется только это
+// };
+
+// // class User {
+// //     name = "Аноним";
+
+// //     sayHi() {
+// //         alert(`Привет, ${this.name}!`);
+// //     }
+// // }
+
+// // new User().sayHi();
+
+// let data = (5 === 5) ? 1 : 0;
+
+
+// // if (result) {
+// //     result = a;
+// // } else {
+// //     result = b;
+// // }
+
+
+
+
+// let state = {
+//     shop: {
+//         nameData: "Алиса",
+//         address: "Ленина 1",
+//         sity: "Питер"
+//     }
+// };
+
+// state.address;
+// state.nameData;
+
+// getName(1333, 2);
+
+// try {
+//     // всё хорошо
+//     // Загружаем график, если не получилось то переходим в catch
+// } catch (e) {
+//     /// Если ошибка то исполняем здесь
+//     // График мы не можем загрузить
+// }
+
+
+// function getName(param1, param2) {
+//     let result = param1 + param2
+
+//     console.log(result);
+// }
+
+// function devParam(param1, param2) {
+//     let result = param1 - param2
+
+//     console.log(result);
+// }
+// let i = 1;
+// i++;
+// i++;
+// i++;
+// i++;
+// i++;
+// i++;
+// i++;
+
+// //console.log('1 nameSecond ' + nameSecond);
+// // getName(1, 2);
+// // getName(2, 23);
+// // getName(14, 22);
+// // getName(11, 267);
+// // devParam(11, 267);
+
+// function yourFunction() {
+//     console.log(' yourFunction');
+// }
+
+// window.onload = function() {
+//     yourFunction();
+// };
+
+
+// //console.log(result);
+// console.log(name);
